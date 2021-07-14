8b4227ed029bb7782eeffac4b1e24e3aa1147e7e
Nelson Gonzalez <njeanpierre23@gmail.com>
2021-07-14 19:58:54 -0400

    CRUD - con Fetch (JSON-Server)

--- src/pages/Tasks/TaskMain.jsx
+++ src/pages/Tasks/TaskMain.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react'
 
+const api4Geek = 'http://localhost:5000/tasks';
+
 const TaskMain = () => {
     // definir todos sus estados
     const [tasks, setTasks] = useState([]);
@@ -8,15 +10,31 @@ const TaskMain = () => {
     const [editSelected, setEditSelected] = useState(null)
     const [editTask, setEditTask] = useState('')
 
+    const getTasks = () => {
+        // PETICION GET PARA OBTENER LA LISTA 
+        fetch(`${api4Geek}`)
+        .then( response => response.json())
+        .then( response => setTasks(response))
+    }
 
     const handleAddTask = event => {
         if(event.key === "Enter"){
             // el spread hace referencia a todas mis tareas actuales + nueva (newTask)
-            setTasks([...tasks,{ id: (tasks.length + 1) , label:  newTask } ]);
+            
+            // POST PARA CREAR
+            fetch(`${api4Geek}`, {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ label: newTask })
+            })
+            .then( response => response.json())
+            .then( response => setTasks([...tasks, response ]))
+            
             setNewTask('');
         }
     }
-
 
     const handleEditTask = (event) => {
         // paso 1: buscar en nuestra lista la posicion que quiero actualizar 
@@ -24,15 +42,28 @@ const TaskMain = () => {
          
         if(event.key === "Enter"){
              
-            setTasks(
-                tasks.map( task => {
-                    if(task.id === editSelected.id){
-                        task.label = editTask
-                    }
-                    return task
-                })
-            )
-            
+
+            // PUT PARA EDITAR
+            fetch(`${api4Geek}/${editSelected.id}`, {
+                method: 'PUT',
+                headers: {
+                  'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ label: editTask })
+            })
+            .then( response => response.json())
+            .then( response => {
+
+                setTasks(
+                    tasks.map( task => {
+                        if(task.id === response.id){
+                            task.label = response.label
+                        }
+                        return task
+                    })
+                )
+            })
+
            setEditTask('')
            setPuedeEditar(false)
         }
@@ -40,15 +71,30 @@ const TaskMain = () => {
 
     }
 
-    const handleDeleteTask = id => setTasks( tasks.filter( task => task.id !== id ))
+    const handleDeleteTask = id => {
+
+        // PARA BORRAR
+        fetch(`${api4Geek}/${id}`, {
+            method: 'DELETE',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+        })
+        .then( response => response.json())
+        .then( response => {
+        
+            setTasks(
+                tasks.filter( task => task.id !== id)
+            )
+        })
+
+    }
     // sus efectos secundarios PUEDO USAR MAS DE UN USEEFFECT
 
     // fase de montaje 
     useEffect(() => {
         if(!tasks.length){
-            setTasks([
-              { id: 1, label: 'Aprendiendo Javascript' }  // hard code
-            ])
+            getTasks()
         }
     }, [tasks.length])
 
@@ -90,7 +136,8 @@ const TaskMain = () => {
                     : null
                 }
             </ul>
-            { puedeEditar? 
+            { puedeEditar?
+
                 <input 
                     type="text" 
                     onChange={(event) =>  setEditTask(event.target.value) } 
